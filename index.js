6695e8b1787e86878ea3ee79eaf92aaf25a56e9f
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2023-12-23 17:21:30 +0800

    Next commit

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function saveBtn(){
-    alert("Hello World!");
-}
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");  
 
-let inputBtn = document.getElementById("input-btn");  
+inputBtn.addEventListener("click", function() {
+    alert("Hello AHH World!");
+    myLeads.push(inputEl.value);
+    console.log(myLeads);    
+});
+
+for (let i = 0; i<myLeads.length; i++)
+{
+    console.log(myLeads[i])
+}
