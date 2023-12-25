5332e5d260d95d1e0d00559194f97c2f664d9936
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2023-12-25 20:28:39 +0800

    Next commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+
+myLeads=JSON.parse(myLeads);
+myLeads.push("www.newlead.com");
+myLeads=JSON.stringify(myLeads);
+console.log(typeof myLeads);
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");   
+const ulEl = document.getElementById("ul-el");    
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
@@ -24,4 +30,4 @@ function renderLead() {
         `
     }
     ulEl.innerHTML = listItems;
-}
+} 
